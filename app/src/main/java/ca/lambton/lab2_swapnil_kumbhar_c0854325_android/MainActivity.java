8760d55656c9eb8d5bd1d916d222c61b0bf6591b
Swapkumbhar31 <swapkumbhar31@gmail.com>
2022-06-13 19:14:31 -0400

    product detail page added

--- app/src/main/java/ca/lambton/lab2_swapnil_kumbhar_c0854325_android/MainActivity.java
+++ app/src/main/java/ca/lambton/lab2_swapnil_kumbhar_c0854325_android/MainActivity.java
@@ -2,6 +2,7 @@ package ca.lambton.lab2_swapnil_kumbhar_c0854325_android;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.room.DatabaseConfiguration;
 import androidx.room.InvalidationTracker;
@@ -9,6 +10,9 @@ import androidx.sqlite.db.SupportSQLiteOpenHelper;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.WindowManager;
 
 import ca.lambton.lab2_swapnil_kumbhar_c0854325_android.Database.ProductDAO;
@@ -26,5 +30,20 @@ public class MainActivity extends AppCompatActivity {
         productRoomDB = ProductRoomDB.getInstance(this);
 
         productRoomDB.productDAO().getAllProducts();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_activity_menu, menu);
+
+        MenuItem getItem = menu.findItem(R.id.goto_list);
+        if (getItem != null) {
+            AppCompatButton button = (AppCompatButton) getItem.getActionView();
+            //Set a ClickListener, the text,
+            //the background color or something like that
+        }
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
