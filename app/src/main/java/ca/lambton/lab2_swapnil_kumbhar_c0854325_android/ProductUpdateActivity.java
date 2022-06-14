7b5fbddc546ef33dd0860f05c610c689bcd9f9cb
Swapkumbhar31 <swapkumbhar31@gmail.com>
2022-06-13 21:53:47 -0400

    edit/add ui added

---
+++ app/src/main/java/ca/lambton/lab2_swapnil_kumbhar_c0854325_android/ProductUpdateActivity.java
@@ -0,0 +1,14 @@
+package ca.lambton.lab2_swapnil_kumbhar_c0854325_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductUpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_update);
+    }
+}
